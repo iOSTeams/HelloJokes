cf8eef87dc572b38a524732336c5169f524d9979
Justin Lee <jlee402@mail.ccsf.edu>
2016-03-13 16:26:14 -0700

    Display text when pressed

--- HelloJokes/ViewController.swift
+++ HelloJokes/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func enterTapped(sender: AnyObject) {
+        label.text = textField.text
     }
 
 
